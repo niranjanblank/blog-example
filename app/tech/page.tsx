686a21c915b063c0ebedd49547fb095f9a575089
Niranjan Shah <niranjanshah474@gmail.com>
2023-07-26 19:10:34 +1000

    Changed some unnecessary flex properties

--- app/tech/page.tsx
+++ app/tech/page.tsx
@@ -27,22 +27,20 @@ export default async function Tech() {
     return (
         <div className="flex flex-col items-center" >
         <h3 className="text-3xl py-1 mt-10">Tech I Use</h3>
-        <div className="flex flex-wrap justify-center items-center gap-10 max-w-4xl">
+        <div className="flex flex-wrap justify-center items-center max-w-4xl">
             {tech_items.map(tech => {
                 return (
 
-                    <div  key={tech.sys.id} className="m-2 w-auto flex">
-                        {/* tech card */}
-                        <div className="text-center shadow-lg p-10 rounded-xl my-10 w-fit  bg-gray-800">
+                    <div className="text-center shadow-lg p-10 rounded-xl my-10 w-fit m-2  bg-gray-800"  key={tech.sys.id}>
                         <div className="w-24 h-24 relative">
                             <Image src={`https://${tech.fields.image.fields.file.url}` as string} fill 
                             sizes="33vw"
                             className="object-contain" alt={tech.fields.alt as string} />
                         </div>
                         <h1 className="text-white">{tech.fields.label as string}</h1>
-                         </div>
-                    </div>
+                    </div> 
                     
+
                 );
             })}
             
